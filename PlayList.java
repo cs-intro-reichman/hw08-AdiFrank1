58381c1c35b3fd04d37b5adbbd7ff90769ac6602
AdiFrank1 <154518396+AdiFrank1@users.noreply.github.com>
2024-02-08 21:38:33 +0200

    edit 12

--- PlayList.java
+++ PlayList.java
@@ -283,7 +283,7 @@ class PlayList {
                 Track temp2 = tracks[i];
 
                 tracks[i] = temp1;
-                tracks [minIndex(i)] = temp2;
+                tracks [minIndex] = temp2;
             }
             
         }
